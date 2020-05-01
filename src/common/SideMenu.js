d86abab99d5e554c4898a42a79d8be117d5f8582
Rishabh <rishabh.tiwari0504@gmail.com>
2020-05-01 06:35:34 +0400

    Added scroll view content.

--- src/common/SideMenu.js
+++ src/common/SideMenu.js
@@ -17,27 +17,28 @@ class SideMenu extends Component {
     render() {
         return (
             <View style={styles.container}>
+                <ImageBackground
+                    styleName="large-banner"
+                    source={{ uri: 'https://shoutem.github.io/static/getting-started/restaurant-2.jpg' }}
+                    style={{ width: windowWidth * 60 / 100 }}
+                >
+                    <Tile >
+                        <Overlay style={{ width: windowWidth * 60 / 100 }}>
+                            <Subtitle styleName="mid">{''}</Subtitle>
+                            <Image
+                                styleName="medium-avatar"
+                                source={{ uri: 'https://scontent.fdxb1-1.fna.fbcdn.net/v/t1.0-9/p960x960/50877592_2588431321242991_5234053253796200448_o.jpg?_nc_cat=103&_nc_sid=85a577&_nc_ohc=NKKSmqks6toAX_mR4yy&_nc_ht=scontent.fdxb1-1.fna&_nc_tp=6&oh=be7bc3f17c3768b5760a3a64af95adf0&oe=5ED0EDC5' }}
+
+                            />
+                            <Subtitle styleName="mid">{''}</Subtitle>
+                            <Subtitle styleName="mid">Rishabh Tiwari</Subtitle>
+                        </Overlay>
+                    </Tile>
+
+                </ImageBackground>
 
                 <ScrollView>
-                    <ImageBackground
-                        styleName="large-banner"
-                        source={{ uri: 'https://shoutem.github.io/static/getting-started/restaurant-2.jpg' }}
-                        style={{ width: windowWidth * 60 / 100 }}
-                    >
-                        <Tile >
-                            <Overlay style={{ width: windowWidth * 60 / 100 }}>
-                                <Subtitle styleName="mid">{''}</Subtitle>
-                                <Image
-                                    styleName="medium-avatar"
-                                    source={{ uri: 'https://scontent.fdxb1-1.fna.fbcdn.net/v/t1.0-9/p960x960/50877592_2588431321242991_5234053253796200448_o.jpg?_nc_cat=103&_nc_sid=85a577&_nc_ohc=NKKSmqks6toAX_mR4yy&_nc_ht=scontent.fdxb1-1.fna&_nc_tp=6&oh=be7bc3f17c3768b5760a3a64af95adf0&oe=5ED0EDC5' }}
 
-                                />
-                                <Subtitle styleName="mid">{''}</Subtitle>
-                                <Subtitle styleName="mid">Rishabh Tiwari</Subtitle>
-                            </Overlay>
-                        </Tile>
-
-                    </ImageBackground>
                     <TouchableOpacity onPress={this.navigateToScreen('Home')}>
                         <View style={styles.viewBox} >
                             <Row styleName="small" >
@@ -59,7 +60,7 @@ class SideMenu extends Component {
                                     styleName="small-avatar rounded-corners"
                                     source={{ uri: 'https://shoutem.github.io/img/ui-toolkit/examples/image-9.png' }}
                                 />
-                                <Text >Profile</Text>
+                                <Subtitle styleName="mid">Profile</Subtitle>
                                 <Icon styleName="disclosure" name="right-arrow" />
                             </Row>
                         </View>
@@ -73,12 +74,50 @@ class SideMenu extends Component {
                                     styleName="small-avatar rounded-corners"
                                     source={{ uri: 'https://shoutem.github.io/img/ui-toolkit/examples/image-9.png' }}
                                 />
-                                <Text >Search</Text>
+                                <Subtitle styleName="mid">Your Orders</Subtitle>
                                 <Icon styleName="disclosure" name="right-arrow" />
                             </Row>
                         </View>
                     </TouchableOpacity>
                     <Divider styleName="line" />
+                    <TouchableOpacity onPress={this.navigateToScreen('Settings')}>
+                        <View >
+                            <Row styleName="small" >
+                                <Image
+                                    styleName="small-avatar rounded-corners"
+                                    source={{ uri: 'https://shoutem.github.io/img/ui-toolkit/examples/image-9.png' }}
+                                />
+                                <Subtitle styleName="mid">Settings</Subtitle>
+                                <Icon styleName="disclosure" name="right-arrow" />
+                            </Row>
+                        </View>
+                    </TouchableOpacity>
+                    <Divider styleName="line" />
+                    <TouchableOpacity onPress={this.navigateToScreen('Settings')}>
+                        <View >
+                            <Row styleName="small" >
+                                <Image
+                                    styleName="small-avatar rounded-corners"
+                                    source={{ uri: 'https://shoutem.github.io/img/ui-toolkit/examples/image-9.png' }}
+                                />
+                                <Subtitle styleName="mid">Settings</Subtitle>
+                                <Icon styleName="disclosure" name="right-arrow" />
+                            </Row>
+                        </View>
+                    </TouchableOpacity>
+                    <Divider styleName="line" />
+                    <TouchableOpacity onPress={this.navigateToScreen('Settings')}>
+                        <View >
+                            <Row styleName="small" >
+                                <Image
+                                    styleName="small-avatar rounded-corners"
+                                    source={{ uri: 'https://shoutem.github.io/img/ui-toolkit/examples/image-9.png' }}
+                                />
+                                <Subtitle styleName="mid">Settings</Subtitle>
+                                <Icon styleName="disclosure" name="right-arrow" />
+                            </Row>
+                        </View>
+                    </TouchableOpacity>
 
                     <TouchableOpacity onPress={this.navigateToScreen('Settings')}>
                         <View >
@@ -87,7 +126,21 @@ class SideMenu extends Component {
                                     styleName="small-avatar rounded-corners"
                                     source={{ uri: 'https://shoutem.github.io/img/ui-toolkit/examples/image-9.png' }}
                                 />
-                                <Text >Settings</Text>
+                                <Subtitle styleName="mid">Notifications</Subtitle>
+                                <Icon styleName="disclosure" name="right-arrow" />
+                            </Row>
+                        </View>
+                    </TouchableOpacity>
+
+                    <Divider styleName="line" />
+                    <TouchableOpacity onPress={this.navigateToScreen('Settings')}>
+                        <View >
+                            <Row styleName="small" >
+                                <Image
+                                    styleName="small-avatar rounded-corners"
+                                    source={{ uri: 'https://shoutem.github.io/img/ui-toolkit/examples/image-9.png' }}
+                                />
+                                <Subtitle styleName="mid">Contact us</Subtitle>
                                 <Icon styleName="disclosure" name="right-arrow" />
                             </Row>
                         </View>
